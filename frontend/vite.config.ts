819d4c24015784722d2da198fd6ab34011255e7a
João Guilherme Barros <jgbarros@gmail.com>
2026-02-02 12:20:52 -0400

    Adicionar Frontend no Docker
    Configuração do Docker e ajuste no Frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,27 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'vendor';
+            }
+            if (id.includes('react-router')) {
+              return 'router';
+            }
+            if (id.includes('primereact')) {
+              return 'primereact';
+            }
+            if (id.includes('axios') || id.includes('rxjs')) {
+              return 'utils';
+            }
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
